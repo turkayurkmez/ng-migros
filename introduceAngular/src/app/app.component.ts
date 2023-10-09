6a10cf21f6aa2cb0a4d54d14eda16402cf7f3a6e
Türkay Ürkmez <turkay@turkayurkmez.com>
2023-10-09 14:50:45 +0300

    one and two way binding explained!

--- introduceAngular/src/app/app.component.ts
+++ introduceAngular/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TodoItem} from './models/todoItem.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'introduce-Angular';
+  title = 'Angular Öğreniyorum';
+  title2 = 'Bu ilk ders...';
+
+  paragraph: string = 'Bu bir paragraf';
+  date: string ='Ekim 2023';
+
+  items: TodoItem[]=[
+    new TodoItem('Çiçekleri sula',false),
+    new TodoItem('Angular Öğren',false),
+    new TodoItem('ASP.NET Core ile api yaz',true)
+  ]
+
+  
 }
---
+++ introduceAngular/src/app/models/todoItem.model.ts
@@ -0,0 +1,6 @@
+export class TodoItem{
+
+    constructor(public name?:string, 
+                public isDone?:boolean)
+    {}
+}
