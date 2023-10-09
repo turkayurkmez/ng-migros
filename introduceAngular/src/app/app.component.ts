571d64d6c8a04f2a48549044277ae11dd0382d59
Türkay Ürkmez <turkay@turkayurkmez.com>
2023-10-09 16:24:28 +0300

    functional binding explained

--- introduceAngular/src/app/app.component.ts
+++ introduceAngular/src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
-import { TodoItem} from './models/todoItem.model';
+import { TodoItem } from './models/todoItem.model';
+
+
+const todoItems: TodoItem[] = [
+  new TodoItem('Çiçekleri sula', false),
+  new TodoItem('Angular Öğren', false),
+  new TodoItem('ASP.NET Core ile api yaz', true)
+]
 
 @Component({
   selector: 'app-root',
@@ -11,13 +18,34 @@ export class AppComponent {
   title2 = 'Bu ilk ders...';
 
   paragraph: string = 'Bu bir paragraf';
-  date: string ='Ekim 2023';
+  date: string = 'Ekim 2023';
 
-  items: TodoItem[]=[
-    new TodoItem('Çiçekleri sula',false),
-    new TodoItem('Angular Öğren',false),
-    new TodoItem('ASP.NET Core ile api yaz',true)
-  ]
+  items: TodoItem[] = todoItems;
 
-  
+  showAllItems: boolean = true;
+  buttonText: string = 'Yapılan işleri gizle';
+
+  filter(): void {
+    this.showAllItems = !this.showAllItems;
+    if (!this.showAllItems) {
+      this.items = this.items.filter(item => !item.isDone);
+      this.buttonText = 'Tümünü göster'
+    }
+    else {
+      this.buttonText = 'Yapılan işleri gizle';
+
+      this.items = todoItems;
+    }
+  }
+
+  getItemsCount(): number {
+    return this.items.filter(item => !item.isDone).length;
+  }
+
+  addNewTask(name:string){
+    let todoItem = new TodoItem(name,false);
+    this.items.push(todoItem);
+  }
+
+
 }
