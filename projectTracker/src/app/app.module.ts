0778d992a2bc9f64b137e25c4e440c8982c14828
Türkay Ürkmez <turkay@turkayurkmez.com>
2023-10-10 11:46:08 +0300

    creating components

--- projectTracker/src/app/app.module.ts
+++ projectTracker/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderMenuComponent } from './header-menu/header-menu.component';
+import { DepartmentMenuComponent } from './department-menu/department-menu.component';
+import { ProjectListComponent } from './project-list/project-list.component';
+import { ProjectComponent } from './project/project.component';
+import { TaskListComponent } from './task-list/task-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderMenuComponent,
+    DepartmentMenuComponent,
+    ProjectListComponent,
+    ProjectComponent,
+    TaskListComponent
   ],
   imports: [
     BrowserModule,
