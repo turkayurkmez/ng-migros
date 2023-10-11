f452fa01880bbb46b451fca2334b5ad023d97987
Türkay Ürkmez <turkay@turkayurkmez.com>
2023-10-11 11:26:34 +0300

    adding API to repo

--- projectTracker/src/app/project-list/project-list.component.ts
+++ projectTracker/src/app/project-list/project-list.component.ts
@@ -1,13 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Project } from '../models/project.model';
 import { projectCollection } from '../models/mocks/projects.mock';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-project-list',
   templateUrl: './project-list.component.html',
   styleUrls: ['./project-list.component.css']
 })
-export class ProjectListComponent {
-    projects: Project[] = projectCollection
-    searchKey: string | undefined; 
+export class ProjectListComponent //implements OnInit  
+{
+  projects: Project[] = projectCollection
+  searchKey: string | undefined;
+
+  constructor(
+    private activeRoute: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.activeRoute.params.subscribe(routeParam => {
+      console.log(routeParam['id']);
+      this.projects = projectCollection;
+      if (routeParam['id'] != undefined) {
+        this.projects = this.projects.filter(p => p.departmentId == routeParam['id'])
+
+      }
+
+    });
+
+  }
+
+  // ngOnInit(){
+
+  // }
+
+
+
+
 }
